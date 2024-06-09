5d4daa917ff5cfa3b1b58951113cf5b31f391168
Ilia Prokhorov <demensdeum@gmail.com>
2024-06-09 22:40:08 +0400

    Pre texture rendering on tiles

--- SurrealEngine/RenderDevice/OpenGL/GLShaderDefinitions.h
+++ SurrealEngine/RenderDevice/OpenGL/GLShaderDefinitions.h
@@ -5,18 +5,18 @@
 const std::string vertexShaderCode =
     R"(#version 100
 attribute vec4 vertex;
-
+attribute vec2 uvIn;
+varying vec2 uvOut;
 void main() {
    gl_Position = vertex;
-}
-)";
+   uvOut = uvIn;
+})";
 
 const std::string fragmentShaderCode = 
-	R"(#version 100
-    precision mediump int;
+    R"(#version 100
     precision mediump float;
-    precision mediump sampler2D;
-    precision mediump samplerCube;
+    varying vec2 uvOut;
+    uniform sampler2D texture;
     void main() {
-    gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        gl_FragColor = texture2D(texture, uvOut);
     })";
