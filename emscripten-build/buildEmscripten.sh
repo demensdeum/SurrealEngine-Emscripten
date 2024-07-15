16d19ae1f1caaa7a43e983056e888a6272498933
Ilia Prokhorov <demensdeum@gmail.com>
2024-07-15 22:46:36 +0400

    html updated

--- emscripten-build/buildEmscripten.sh
+++
@@ -1,4 +0,0 @@
-emcmake cmake --fresh .. && emmake make clean && emmake make -j 32
-cp SurrealEngine.data /srv/http/SurrealEngine/SurrealEngine.data
-cp SurrealEngine.js /srv/http/SurrealEngine/SurrealEngine.js
-cp SurrealEngine.wasm /srv/http/SurrealEngine/SurrealEngine.wasm
--- emscripten-build/buildEmscriptenQuick.sh
+++
@@ -1,5 +0,0 @@
-emcmake cmake ..
-emmake make -j 32
-cp SurrealEngine.data /srv/http/SurrealEngine/SurrealEngine.data
-cp SurrealEngine.js /srv/http/SurrealEngine/SurrealEngine.js
-cp SurrealEngine.wasm /srv/http/SurrealEngine/SurrealEngine.wasm
